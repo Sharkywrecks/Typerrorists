a6ee8ffa88d523ded69633fb0aac3282e966623d
Sharkywrecks <104490816+Sharkywrecks@users.noreply.github.com>
2025-02-02 04:49:30 +0000

    Register route

--- client/src/app/account/account.routes.ts
+++ client/src/app/account/account.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
+import { RegisterComponent } from './register/register.component';
 
 export const ACCOUNT_ROUTES: Routes = [
   {path: 'login', component: LoginComponent},
-  { path: 'verify-email', component: VerifyEmailComponent }
+  { path: 'verify-email', component: VerifyEmailComponent },
+  { path: 'register', component: RegisterComponent }
 ];
